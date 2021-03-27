0ff4f4b2bdfbc9f8e3ae85d72197f3800656d5db
Silvan Gehrig <silvan.gehrig@ost.ch>
2021-03-27 19:23:03 +0100

    Add create component for workspace command.

---
+++ ng-create-component.ps1
@@ -0,0 +1,10 @@
+# -> Enable PS1 Execution (run command as Admin): [ Set-ExecutionPolicy -ExecutionPolicy Unrestricted ]
+# -> Usage / Run from CMD: [ powershell -file ng-create-app.ps1 new-app-name ]
+param(
+  [Parameter(Mandatory=$True)]
+  [ValidateNotNull()]
+  $appname
+)
+
+cd ./samples
+npx ng generate component --project $appname
